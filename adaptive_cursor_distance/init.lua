f327bc8b7c08f37e68d7588d1281c43616ee4187
gjchangmu <gjchangmu@gmail.com>
2020-05-05 14:40:12 +0800

    .

--- adaptive_cursor_distance/init.lua
+++ adaptive_cursor_distance/init.lua
@@ -1,6 +1,13 @@
+dofile( "data/scripts/lib/utilities.lua" )
+
 function OnPlayerSpawned( player_entity )
+
+	local reticlecomp = EntityGetFirstComponent(player_entity, "SpriteComponent", "aiming_reticle")
+	EntitySetComponentIsEnabled( player_entity, reticlecomp, false )
+	
 	if tonumber(StatsGetValue("playtime")) > 1 then return end
 
 	local x, y = EntityGetTransform(player_entity)
 	local adaptive_cursor = EntityLoad("mods/adaptive_cursor_distance/files/entities/adaptive_cursor.xml", x, y)
+	
 end
