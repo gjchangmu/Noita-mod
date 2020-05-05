8af00712a883db8cf328681bd174af63300d96b5
gjchangmu <gjchangmu@gmail.com>
2020-05-05 10:59:32 +0800

    working. but planning to change to aim on enemy on aim

--- adaptive_cursor_distance/files/scripts/update_adaptive_cursor.lua
+++ adaptive_cursor_distance/files/scripts/update_adaptive_cursor.lua
@@ -13,9 +13,17 @@ local cxym = math.sqrt(cx * cx + cy * cy)
 cx = cx / cxym
 cy = cy / cxym
 
+local mind2 = 15625
+local enemies = EntityGetWithTag("enemy")
+for i, enemy in ipairs(enemies) do
+	local ex, ey, erot, escale_x, escale_y = EntityGetTransform(enemy)
+	local ed2 = (ex - x) * (ex - x) + (ey - y) * (ey - y)
+	if ed2 < mind2 then mind2 = ed2 end
+end
+if mind2 < 900 then mind2 = 900 end
+d = math.sqrt(mind2) - 10
+EntitySetTransform(cursor, x + cx * d, y + cy * d)
+
 if( Random( 1, 100 ) <= 2 ) then
 	GamePrint(cx .. ", " .. cy)
 end
-
-d = 45
-EntitySetTransform(cursor, x + cx * d, y + cy * d)
